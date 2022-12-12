9f29de4cb9dfef40ecfe28af51181072ac0b11f8
Roma-Leto <119625441+roma-leto@users.noreply.github.com>
2022-12-12 22:08:46 +0300

    Form

--- lending/views.py
+++ lending/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
+from .forms import MsgForm
+from .models import MessageClient
 
 def index(request):
     return render(request, 'lending/index.html')
 
 
+# class MsgCreateView(CreateView):
+#     template_name = 'index.html'
+#     form_class = MsgForm
+#     success_url = ''
+
+
 def message_sent(request):
+    # mess = request.POST['message']
+    # print(mess)
+    # if request.method == 'POST':
+    #     msgf = MsgForm(request.POST)
+    #     if msgf.is_valid():
+    #         msgf.save()
+    #         print(msgf.name_user)
+    #     print(dir(msgf))
+    msgf = MsgForm(request.GET)
+    mc = MessageClient(
+        name_user=msgf.name_user,
+        email_user=msgf.email_user,
+        subject_message=msgf.subject_message,
+        message=msgf.message
+    )
+    mc.save()
     return render(request, 'lending/msg-sent.html')
